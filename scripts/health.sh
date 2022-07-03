64a42cbf6c7c285bf5c5603f171d54d57e9cd09f
bgwb0 <bgwb0007@naver.com>
2022-07-03 14:37:27 +0900

    appspec.yml 수정

--- scripts/health.sh
+++ scripts/health.sh
@@ -35,5 +35,9 @@ do
   fi
 
   echo "> Health check 연결 실패. 재시도..."
+  echo "> ${UP_COUNT}"
+  echo "> Health Check Start!"
+  echo "> IDLE_PORT: $IDLE_PORT"
+  echo "> curl -s http://localhost:$IDLE_PORT/profile "
   sleep 10
 done
