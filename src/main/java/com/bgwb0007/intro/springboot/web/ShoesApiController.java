9d3766fa2566cb1de1a7e7fb050101c6f1e9da1e
bgwb0 <bgwb0007@naver.com>
2022-07-10 18:56:11 +0900

    daily에 shoes 프론트 적용

--- src/main/java/com/bgwb0007/intro/springboot/web/ShoesApiController.java
+++ src/main/java/com/bgwb0007/intro/springboot/web/ShoesApiController.java
@@ -1,23 +1,10 @@
 package com.bgwb0007.intro.springboot.web;
 
 import com.bgwb0007.intro.springboot.service.ShoesService;
-import com.bgwb0007.intro.springboot.util.FileStore;
-import com.bgwb0007.intro.springboot.web.dto.ShoesSaveRequestDto;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.util.List;
 import java.util.Map;
-
-
 
 @RequiredArgsConstructor
 @RestController
